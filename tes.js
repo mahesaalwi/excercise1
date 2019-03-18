dd0c8d6a2ad8f080e654c64a11ba50f811f727ec
Hesa <hesa@hesa.local>
2019-03-18 22:48:06 +0700

    upload latihan

---
+++ tes.js
@@ -0,0 +1,85 @@
+
+//footer class
+ 
+anime({
+    targets: 'div.mag1',
+    translateY: 100,
+    value:200,
+  delay:200,
+    rotate: '1turn',
+  
+    duration: 80000
+  });
+
+  
+anime({
+    targets: 'div.mag2',
+    translateY: 100,
+    rotate: '1turn',
+  
+    duration: 30000
+  });
+
+  
+anime({
+    targets: 'div.mag3',
+    translateY: 100,
+   
+  
+    duration: 40000
+  });
+
+  
+anime({
+    targets: 'div.mag4',
+    translateY: 100,
+  
+  
+    duration: 30000
+  });
+
+  //flood-class jumbotron
+   
+anime({
+    targets: 'div.flood',
+    translateY: 500,
+  
+  autoplay:true,
+  delay:500,
+  
+  
+    duration: 18000
+  });
+
+     
+anime({
+    targets: 'div.flood1',
+    translateY: 500,
+    delay: 800,
+ 
+
+   
+    duration: 18000
+  });
+
+  anime({
+    targets: 'div.flood2',
+    translateY: 500,
+    
+  
+    
+    duration: 18000
+  });
+
+  anime({
+    targets: 'div.flood3',
+    translateY: 500,
+    delay: 800,
+    
+    duration: 18000
+    
+  });
+
+
+
+
